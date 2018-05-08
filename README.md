0e16b639fdd59b48124797b8a587dfab43050950
REDDY PRASAD <dev.drprasad@aim.com>
2018-05-08 12:29:08 +0530

    :sparkles: Custom domain support. Search via query param

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # stephanie-ui
+
+## References
+
+1. https://stackoverflow.com/questions/21088250/android-launch-twitter-intent
+2. https://stackoverflow.com/questions/33233966/android-share-intent-twitter-how-to-share-only-by-tweet-or-direct-message
+3. https://developer.chrome.com/multidevice/android/intents
+4. https://stackoverflow.com/questions/8974705/how-to-open-twitter-page-in-twitter-app-from-my-iphone-app
+5. https://docs.aws.amazon.com/Route53/latest/DeveloperGuide/migrate-dns-domain-inactive.html
+
+6. https://stackoverflow.com/questions/44176167/dynamo-db-query-using-contains-operator?rq=1
+7. https://stackoverflow.com/questions/35652668/dynamo-db-contains
+8. https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html#scan-property
--- app.js
+++ app.js
@@ -13,7 +13,14 @@ app.set('view engine', 'handlebars');
 app.get('/healthcheck', (req, res) => res.send('I am doing fine! 😎 '));
 
 app.get('/', (req, res) => {
-  dynamodb.scan({ TableName: 'mentors' }, (err, response) => {
+  const options = { TableName: 'mentors' };
+  if (req.query.q) {
+    options.FilterExpression = 'contains(tweet, :search) OR contains(bio, :search)';
+    options.ExpressionAttributeValues = { ':search': req.query.q.trim() };
+  }
+
+  dynamodb.scan(options, (err, response) => {
+    if (err) console.log(err, err.stack);
     const mentors = response.Items.map(mentor => Object.assign(mentor, {
       twitter_url: `https://twitter.com/${mentor.username}`,
       tweet: mentor.tweet.trim().replace(/https:\/\/t\.co\/\w+$/, ''),
@@ -22,6 +29,7 @@ app.get('/', (req, res) => {
           ? mentor.profile_image.replace(/(\.[A-Za-z]{3,4})$/, '_bigger$1')
           : 'https://abs.twimg.com/sticky/default_profile_images/default_profile.png',
     }));
+    console.log(`Found ${mentors.length} for given search criteria q: ${req.query.q}`);
     res.render('home', { mentors });
   });
 });
